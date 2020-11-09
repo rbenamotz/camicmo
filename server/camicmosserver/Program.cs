072bafe7034ba8b3f47abfe7bde2bec835b8a420
Roey Benamotz <roey@benamotz.com>
2020-11-08 21:45:36 -0500

    folders

---
+++ server/camicmosserver/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace camicmosserver
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            var s = new SerialHandler();
+            while (true)
+            {
+                s.SetColors("gg");
+                Thread.Sleep(500);
+                s.SetColors("gb");
+                Thread.Sleep(500);
+                s.SetColors("br");
+                Thread.Sleep(500);
+                s.SetColors("rr");
+                Thread.Sleep(500);
+                s.SetColors("r ");
+                Thread.Sleep(500);
+                s.SetColors("  ");
+                Thread.Sleep(500);
+                s.SetColors(" g");
+                Thread.Sleep(500);
+
+            }
+
+        }
+
+        static void RunService()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+
+        }
+    }
+}
---
+++ server/camicmosserver/SerialHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace camicmosserver
+{
+    class SerialHandler
+    {
+        // Create the serial port with basic settings
+        private SerialPort port = new SerialPort("COM5",115200, Parity.None, 8, StopBits.One);
+
+        public SerialHandler()
+        {
+            var ff = SerialPort.GetPortNames();
+
+            port.Open();
+        }
+
+        public void SetColors(string s)
+        {
+            port.Write(s);
+        }
+    }
+}
